bff1884e9a444fba2cb02071c2ff5ba90813292c
babofitos <tehchoi@gmail.com>
2013-06-18 02:16:12 -0400

    button frontend

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,5 +1,11 @@
 function IndexCtrl($scope, $http, $location) {
   $http.get('/list').success(function(data, status) {
+    for (var i=0;i<data.length;i++) {
+      data[i].edit = false
+      data[i].submit = true
+      data[i].delete = false
+      data[i].cancel = true
+    }
     $scope.loots = data
   })
 
@@ -18,12 +24,22 @@ function IndexCtrl($scope, $http, $location) {
     })
   }
 
-  $scope.edit = function() {
-    $http.put()
+  $scope.edit = function(loot) {
+    loot.edit = true
+    loot.submit = false
+    loot.delete = true
+    loot.cancel = false
   }
 
-  $scope.delete = function() {
+  $scope.delete = function(loot) {
     $http.delete()
+  }
+
+  $scope.cancel = function(loot) {
+    loot.edit = false
+    loot.submit = true
+    loot.delete = false
+    loot.cancel = true
   }
 }
 
